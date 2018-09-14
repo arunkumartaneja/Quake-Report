33ec265189258b0449ee195a40edc5836a6dd86c
arun.taneja@sgainc.com <arun.taneja@sgainc.com>
2018-09-14 12:30:35 +0530

    added loader

---
+++ app/src/main/java/com/example/android/quakereport/activity/EarthquakeLoader.java
@@ -0,0 +1,38 @@
+package com.example.android.quakereport.activity;
+
+import android.content.Context;
+import android.content.AsyncTaskLoader;
+
+import com.example.android.quakereport.model.Earthquake;
+import com.example.android.quakereport.util.QueryUtils;
+
+import java.util.List;
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    /**
+     * Query URL
+     */
+    private String mUrl;
+
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
